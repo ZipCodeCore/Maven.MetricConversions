69260aeebd721e8068e0d2b258e79322b9c6401e
Brian Mullin <brianmullin@zipcoder6.local>
2017-05-12 14:49:32 -0400

    Halfway

--- src/main/java/ConversionTool.java
+++ src/main/java/ConversionTool.java
@@ -3,19 +3,58 @@ public class ConversionTool {
 
     public static void main(String[] args){}
 
-    public static float CentimetersToInches(float centimeters){}
+   /* public float inches = 0f;
+    public float centimeters;
+    public float feet;
+    public float meters;
+    public float celsius;
+    public float fahrenheit;
+    public float mph;
+    public float kph;*/
 
-    public static float InchesToCentimeters(float inches){}
+    public static float CentimetersToInches(float centimeters){
 
-    public static float FeetToMeters(float feet){}
+        float inches = Math.max(0, centimeters) * (1f / 2.54f);
 
-    public static float MetersToFeet(float meters){}
+        return inches;
 
-    public static float CelsiusToFahrenheit(float celsius){}
+    }
 
-    public static float FahrenheitToCelsius(float fahrenheit){}
+    public static float InchesToCentimeters(float inches){
 
-    public static float MphToKph(float mph){}
+        float centimeters = Math.max(0, inches) * 2.54f;
 
-    public static float KphToMph(float kph){}
+        return centimeters;
+    }
+
+    public static float FeetToMeters(float feet){
+
+        float meters = Math.max(0, feet) * 0.3048f;
+
+        return meters;
+
+    }
+
+    public static float MetersToFeet(float meters){
+
+        float feet = Math.max(0, meters) * 3.28084f;
+        return feet;
+
+    }
+
+    public static float CelsiusToFahrenheit(float celsius){
+        return 1;
+    }
+
+    public static float FahrenheitToCelsius(float fahrenheit){
+        return 1;
+    }
+
+    public static float MphToKph(float mph){
+        return 1;
+    }
+
+    public static float KphToMph(float kph){
+        return 1;
+    }
 }
