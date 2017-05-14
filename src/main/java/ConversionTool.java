185a8b69ad0ef40e6c66efda312e44711408e115
Justin Haas <justinmhaas1@gmail.com>
2017-05-13 22:40:21 -0400

    finished conversion tool

--- src/main/java/ConversionTool.java
+++ src/main/java/ConversionTool.java
@@ -1,25 +1,71 @@
 public class ConversionTool {
 
 
-    public static void main(String[] args){}
-
-    public static float CentimetersToInches(float centimeters){
-
-
-
+    public static void main(String[] args) {
     }
 
-    public static float InchesToCentimeters(float inches){}
+    public static float CentimetersToInches(float centimeters) {
 
-    public static float FeetToMeters(float feet){}
+        if(centimeters < 0){
+            return 0;
+        }
 
-    public static float MetersToFeet(float meters){}
+        return centimeters / 2.54f;
+    }
 
-    public static float CelsiusToFahrenheit(float celsius){}
 
-    public static float FahrenheitToCelsius(float fahrenheit){}
+    public static float InchesToCentimeters(float inches){
 
-    public static float MphToKph(float mph){}
+        if (inches < 0){
+            return 0;
+        }
 
-    public static float KphToMph(float kph){}
+        return inches * 2.54f;
+    }
+
+    public static float FeetToMeters(float feet){
+
+        if(feet < 0){
+            return 0;
+        }
+
+        return feet / 3.28f;
+    }
+
+    public static float MetersToFeet(float meters){
+
+        if(meters < 0){
+            return 0;
+        }
+
+        return meters * 3.28084f;
+    }
+
+    public static float CelsiusToFahrenheit(float celsius){
+
+        return (celsius * 1.8f) + 32f;
+    }
+
+    public static float FahrenheitToCelsius(float fahrenheit){
+
+        return (fahrenheit -32f) * .5556f;
+    }
+
+    public static float MphToKph(float mph){
+
+        if (mph < 0){
+            return 0;
+        }
+
+        return mph * 1.60934f;
+    }
+
+    public static float KphToMph(float kph){
+
+        if (kph < 0){
+            return 0;
+        }
+
+        return kph * .6214f;
+    }
 }
